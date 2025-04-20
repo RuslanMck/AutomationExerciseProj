bf667945b173bec5e0abb1121bffe1f2618c7928
Ruslan Marchuk <mckruslan@gmail.com>
2025-04-20 13:34:06 +0200

    Fixing issue with Selenide browser for Git Actions

--- src/main/java/pages/GeneralElements.java
+++ src/main/java/pages/GeneralElements.java
@@ -4,6 +4,8 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.appear;
+
 public class GeneralElements {
     private SelenideElement welcomeModal = Selenide.$(".fc-dialog-container");
     private SelenideElement consentButton = Selenide.$(".fc-cta-consent");
@@ -13,8 +15,8 @@ public class GeneralElements {
     }
 
     public void clickConsentButton(){
-        welcomeModal.shouldBe(Condition.visible);
-        consentButton.shouldBe(Condition.visible);
-        consentButton.click();
+        if (welcomeModal.exists()&&consentButton.exists()) {
+            consentButton.should(appear).click();
+        }
     }
 }
