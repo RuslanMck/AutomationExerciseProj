b39a28007a938ecfd5013a784899c234939e3dd2
Ruslan Marchuk <mckruslan@gmail.com>
2025-04-20 13:12:43 +0200

    Fixing issue with Selenide browser for Git Actions

--- src/test/java/ui/HomePageTest.java
+++ src/test/java/ui/HomePageTest.java
@@ -2,6 +2,7 @@ package ui;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -13,7 +14,16 @@ public class HomePageTest {
 @BeforeClass
 public void setUp(){
     Configuration.browserSize = "1920x1080";
-    Configuration.holdBrowserOpen = true;
+    Configuration.holdBrowserOpen = false; // Headless = no need to hold open
+    Configuration.headless = true;         // Run in headless mode for CI
+    Configuration.browser = "chrome";
+    Configuration.browserCapabilities = new ChromeOptions()
+            .addArguments("--disable-dev-shm-usage")
+            .addArguments("--no-sandbox")
+            .addArguments("--disable-gpu")
+            .addArguments("--remote-allow-origins=*")
+            .addArguments("--user-data-dir=/tmp/chrome-profile-" + System.currentTimeMillis());
+
     Selenide.open(HomePage.BASE_URL);
 }
 
